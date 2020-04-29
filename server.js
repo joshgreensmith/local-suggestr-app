ce35aec60e910354cd0e7a10eefeaa076bb9e371
Josh Greensmith <joshgreensmith@gmail.com>
2020-04-29 20:04:49 +0100

    Updated to just using git

---
+++ server.js
@@ -0,0 +1,187 @@
+//server.js
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+// const port = process.env.PORT || 8080;
+const port = 8080;
+const app = express();
+
+// Custom modules for data processing
+const bodyParser = require('body-parser');
+const _ = require('underscore');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Test ping route
+app.get('/ping', function (req, res) {
+    console.log('Ping request received!');
+    return res.send('pong');
+});
+
+// Route to get all questions from the question file
+app.get('/api/getAllQuestions', function(request, response) {
+    var questions_raw = fs.readFileSync('all_questions_2.json');
+    var questions_json = JSON.parse(questions_raw);
+    response.send(questions_json);
+});
+
+// Return true or false on whether to ignore id from recommendations pool
+// Go through each question response, check if answered and see if it should be ignored
+function ignore_id(movie, questions) {
+    // Age rating
+    if('User response' in questions['1']) {
+        if(!(questions['1']['User response'] == "Don't mind")) {
+            if(movie['Rated'] == 'PG-13') {
+                if(!(questions['1']['User response'] == 'PG-13')) {return [true, 1]}
+            }
+            if(movie['Rated'] == 'R') {
+                if(!(questions['1']['User response'] == 'R')) {return [true, 1]}
+            }
+        }      
+    }
+
+    // Genre
+    var test_genre;
+    var found_genre = false;
+    if('User response' in questions['2']) {
+        for(i = 0; i < questions['2']['User response'].length; i++) {
+            test_genre = questions['2']['User response'][i];
+            if(movie['Genre'].includes(test_genre)) {found_genre = true}
+        }
+        if(!(found_genre)) {return [true, 2]}
+    }
+
+    // Film length
+    if('User response' in questions['3']) {
+        if(!(questions['3']['User response'] == "Don't mind")) {
+            if(questions['3']['User response'] == 'Over two hours') {
+                if(!(movie['Runtime'] > 120)) {return [true, 3]}
+            }
+            if(questions['3']['User response'] == 'Under two hours') {
+                if(!(movie['Runtime'] < 120)) {return [true, 3]}
+            }
+        }
+    }
+
+    // Language
+    var found_language = false;
+    if('User response' in questions['4']) {
+        for(i = 0; i < questions['4']['User response'].length; i++) {
+            test_language = questions['4']['User response'][i]
+            if(movie['Languages'].includes(test_language)) {found_language = true}
+        }
+        if(!(found_language)) {return [true, 4]}
+    }
+
+    // Film date
+    if('User response' in questions['5']) {
+        if(!(movie['Year'] > questions['5']['User response'])) {return [true, 5]}
+    }
+
+    // Mainstream studio
+    if('User response' in questions['6']) {
+        if(!(questions['6']['User response'] == "Don't mind")) {
+            if(questions['6']['User response'] == 'Yes') {
+                if(!(movie['BigFiveStudio'])) {return [true, 6]}
+            }
+            if(questions['6']['User response'] == 'No') {
+                if(movie['BigFiveStudio']) {return [true, 6]}
+            }
+        }    
+    }
+
+    // Oscar nominated
+    if('User response' in questions['7']) {
+        if(!(questions['6']['User response'] == "Don't mind")) {
+            if(questions['7']['User response'] == 'Yes') {
+                if(!(movie['OscarNominated'])) {return [true, 7]}
+            }
+            if(questions['7']['User response'] == 'No') {
+                if(movie['OscarNominated']) {return [true, 7]}
+            }
+        }    
+    }
+
+    return false
+}
+
+function get_recommend_data(imdb_ids) {
+    recommend_data = []
+
+    var recc_db = JSON.parse(fs.readFileSync('db/recc_db_v1.json'));
+
+    for(i = 0; i < imdb_ids.length; i++) {
+        imdbID = imdb_ids[i];
+        recommend_data.push(recc_db[imdbID]);
+    }
+
+    return recommend_data
+}
+// Route to get recommended films based on question answers given back
+app.post('/api/getAllRecommendations', function(request, response) {
+    const questions = request.body;
+    
+    var movies = JSON.parse(fs.readFileSync('db/core_db_v4.json'));
+    const number_of_movs_in_db = Object.keys(movies).length;
+
+    var imdbIDs_to_ignore = [];
+    var movie;
+    var ignore_id_test;
+    // Overall loop through movies to reduce processing time overall - change fxn to process one film maybe
+    for(j = 1; j <= number_of_movs_in_db; j++) {
+        movie = movies[j.toString()]
+        ignore_id_test = ignore_id(movie, questions);
+
+        if(ignore_id_test[0]) {
+            imdbIDs_to_ignore.push(movie['imdbID']);
+        }
+    }
+
+    // Work out which titles to return based on question responses
+    ids_to_recommend = []
+    for(i = 1; i <= number_of_movs_in_db; i++) {
+        movie = movies[i.toString()];
+        if(!(imdbIDs_to_ignore.includes(movie['imdbID']))) {
+            ids_to_recommend.push(movie['imdbID']);
+        }
+    }
+
+    // If more than 5 movies returned, randomly choose 5 movies to show
+    if(ids_to_recommend.length > 6) {
+        ids_to_recommend = _.shuffle(ids_to_recommend).slice(0,6);
+    }
+
+    recommend_data = get_recommend_data(ids_to_recommend)
+
+    response.send(recommend_data);
+});
+
+// Route to get recommended films based on question answers given back
+app.post('/api/getPoolSize', function(request, response) {
+    const questions = request.body;
+    
+    var title_json = JSON.parse(fs.readFileSync('db/title_db.json'));
+    var movies = JSON.parse(fs.readFileSync('db/core_db_v4.json'));
+    const number_of_movs_in_db = Object.keys(movies).length;
+
+    var imdbIDs_to_ignore = [];
+
+    var movie;
+    var ignore_id_test;
+    // Overall loop through movies to reduce processing time overall - change fxn to process one film maybe
+    for(j = 1; j <= number_of_movs_in_db; j++) {
+        movie = movies[j.toString()]
+        ignore_id_test = ignore_id(movie, questions);
+
+        if(ignore_id_test[0]) {
+            imdbIDs_to_ignore.push(movie['imdbID']);
+        }
+    }
+    var pool_size = number_of_movs_in_db - imdbIDs_to_ignore.length
+    response.send({pool_size});
+});
+
+// Listen on port given by Heroku or the local port
+app.listen(port);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,333 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import OpenSnapper from './OpenSnapper.png'
+import ClosedSnapper from './ClosedSnapper.png'
+
+class Question extends React.Component {
+    render() {
+        return (
+            <div className = "Question">
+                {this.props.title_text}
+            </div>
+        )
+    }
+}
+
+class ResponseBox extends React.Component {
+    render() {
+        const selectedResponses = this.props.selectedResponses;
+        console.log(selectedResponses);
+        const responses = this.props.responses.map(response => {
+            return (
+                <ResponseButton
+                response_text = {response}
+                key = {response}
+                isSelected = {selectedResponses.includes(response)}
+                onClick={() => this.props.onClick(response)}
+                />
+            )
+        });    
+        return (
+            <div className = 'ResponseBox'>
+                {responses}
+            </div>
+        )
+    }
+}
+
+class ResponseButton extends React.Component {
+    render() {
+        return (
+            <div className = 'ResponseButtonContainer'
+            onClick = {this.props.onClick}>
+                <img src = {this.props.isSelected ? ClosedSnapper : OpenSnapper}
+                className = 'SnapperImage'
+                alt = 'Error: Image not loaded'/>
+
+                <div className = 'ResponseButtonText'>{this.props.response_text}</div>
+            </div>
+            
+        )
+    }
+}
+
+class NextButton extends React.Component {
+    render() {
+        return(
+            <div className = 'NextButton'
+            onClick = {() => this.props.onClick()}>
+                Next
+            </div>
+        )
+    }
+}
+
+class RecommendationsBox extends React.Component {
+    render() {
+        if (this.props.recommendations.length > 0) {
+            const recommendations = this.props.recommendations.map(recommendation => {
+                return(
+                    <Recommendation key = {recommendation['Title']}
+                    data = {recommendation} />
+                )
+            });
+            return (
+                <div className = 'RecommendationsBox'>
+                    {recommendations}
+                </div>
+            )
+        }
+        else {
+            return(
+                <div>No recommendations found!</div>
+            )
+        }
+    }
+}
+
+class Recommendation extends React.Component {
+    render() {
+        const data = this.props.data;
+        return (
+            <div className = 'RecommendationContainer'>
+                <img src = {OpenSnapper}
+                className = 'SnapperImage'
+                alt = 'Error: Image not loaded'/>
+
+                <div className = 'RecommendationData'>
+                    <div>
+                        <img className ='PosterImage' 
+                        src = {data['Poster']} 
+                        alt = 'No image found'/>
+                    </div>
+
+                    <div className = 'RecommendationTextDiv'>
+                        <b>{this.props.data['Title']}</b>
+                        <p>IMDB rating = {data['imdbRating']}</p>
+                        <p>{data['Plot']}</p>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+class PoolSizeBox extends React.Component {
+    render() {
+        return (
+            <div className = 'PoolSizeBox'>
+                Sampling from {this.props.pool_size} films!
+            </div>
+        )
+    }
+}
+
+class SuggestrApp extends React.Component {
+    // Initialise the state
+    constructor(props) {
+        super(props);
+        this.state = {
+            question_number: 0,
+            pool_size: 10081,
+            completed_all_qs: false,
+            got_responses: false,
+            all_questions: {
+                "0":{
+                    "Question":  "Answer 10 questions and get a film you actually want to watch!",
+                    "Responses": ["Begin"]
+                }
+            }
+        };
+    }
+
+    // Get all questions on first mount
+    componentDidMount() {
+        this.getQuestions();
+        document.title = "Suggestr";
+    }
+
+    getQuestions = () => {
+        fetch('/api/getAllQuestions')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({ all_questions: data }, () => {
+                console.log('Questions loaded');
+            });
+        });
+    }
+    
+    getPoolSize = () => {
+        const all_questions = this.state.all_questions;
+        fetch('/api/getPoolSize', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(all_questions),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                pool_size: data['pool_size']
+            })
+        })
+        .catch((error) => {
+            console.error('Error', error);
+        })
+    }
+
+    getRecommendations = () => {
+        const all_questions = this.state.all_questions;
+        fetch('/api/getAllRecommendations', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(all_questions),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                got_responses: true,
+                recommendations: data
+            })
+        })
+        .catch((error) => {
+            console.error('Error', error);
+        })
+    }
+
+    // Get responses for dynamic questions
+    getResponses() {
+
+    }
+
+    handleClick(response) {
+        // Deal with multiple reponse questions differently to single response questions
+        const cur_q_num = this.state.question_number;
+        const cur_q_num_str = this.state.question_number.toString();
+        const cur_all_qs = this.state.all_questions;
+        const cur_q = cur_all_qs[cur_q_num_str];
+        const cur_q_multiple_responses = cur_q['Multiple responses']
+
+        if(cur_q_multiple_responses) {
+            // Go to next question if next button selected, don't need to update responses (add check for any response)
+            if (response == 'Next') {
+                // Check that we haven't reached the end of the questions and update this.state.completed_all_qs if so
+                if(cur_q_num === 7) {
+                    this.setState({completed_all_qs: true});
+                    this.getRecommendations();
+                }
+                else {
+                    this.setState( {question_number: cur_q_num + 1} );
+                }
+            } else {
+                // Add response to array in current question
+                if('User response' in cur_q) {
+                    // Check if response already in user response and remove it if so
+                    if(cur_q['User response'].includes(response)) {
+                        // Remove response for user response
+                        var new_responses = cur_q['User response'];
+                        new_responses = new_responses.filter(e => e !== response);
+                        var new_all_qs = cur_all_qs;
+                        new_all_qs[cur_q_num_str]['User response'] = new_responses;
+                    } else {
+                        var new_responses = cur_q['User response'];
+                        new_responses.push(response);
+                        var new_all_qs = cur_all_qs;
+                        new_all_qs[cur_q_num_str]['User response'] = new_responses;
+                    }               
+                } else {
+                    // Create array with one element of the first response
+                    var new_all_qs = cur_all_qs;
+                    new_all_qs[cur_q_num_str]['User response'] = [response];
+                }
+                this.setState({all_questions: new_all_qs});
+            }
+        } else {
+             // Add user response to all_questions and update question_number when an option is selected
+            var new_all_qs = cur_all_qs;
+            new_all_qs[cur_q_num_str]['User response'] = response;
+            // var response_index = cur_all_qs[cur_q_num_str]['Responses'].indexOf(response);
+            // new_all_qs[cur_q_num_str]['User response index'] = response_index;
+                    
+            // Check that we haven't reached the end of the questions and update this.state.completed_all_qs if so
+            if(cur_q_num === 7) {
+                this.setState({completed_all_qs: true});
+                this.getRecommendations();
+            }
+            else {
+                this.setState( {all_questions: new_all_qs} );
+                this.setState( {question_number: cur_q_num + 1} );
+            } 
+        }
+        
+        this.getPoolSize();
+    }
+
+    render() {
+        const completed_all_qs = this.state.completed_all_qs;
+        const got_responses = this.state.got_responses;
+        const recommendations = this.state.recommendations;
+        if(completed_all_qs) {
+            if(got_responses) {
+                return (
+                    <div className = 'SuggestrApp'>
+                        <Question title_text='Recommendations'/>
+                        <RecommendationsBox
+                        recommendations = {recommendations}
+                        />
+                    </div>
+                )
+            } else {
+                return (
+                    <div className ='LoadingResponses'>Loading responses</div>
+                )
+            }
+        } else {
+            const all_qs = this.state.all_questions;
+            const q_num = this.state.question_number;
+            const q = all_qs[q_num.toString()];
+            const q_text = q['Question'];
+            const multiple_responses = q['Multiple responses'];
+            const responses_arr = q['Responses'];
+            const pool_size = this.state.pool_size;
+
+
+            if(multiple_responses) {
+                return(
+                    <div className = 'SuggestrApp'>
+                        <Question title_text={q_text}/>
+                        <PoolSizeBox pool_size={pool_size}/>
+                        <ResponseBox responses={responses_arr}
+                        selectedResponses = {'User response' in q ? q['User response'] : []}
+                        onClick = {response => this.handleClick(response)}/>
+                        <NextButton onClick = {() => this.handleClick('Next')}/>
+                    </div>
+                )
+            } else {
+                return(
+                    <div className = 'SuggestrApp'>
+                        <Question title_text={q_text}/>
+                        <PoolSizeBox pool_size={pool_size}/>
+                        <ResponseBox responses={responses_arr}
+                        selectedResponses = {[]}
+                        onClick = {response => this.handleClick(response)}/>
+                    </div>
+                ) 
+            }     
+        }
+        
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <SuggestrApp />,
+    document.getElementById('root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
